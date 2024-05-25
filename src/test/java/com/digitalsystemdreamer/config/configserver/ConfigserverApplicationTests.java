4ab2ddbd569dc6e2dd16785cc08e92c5618d61b1
digitalsystemdreamer <digitalsystemdreamer@gmail.com>
2024-05-25 18:18:03 +0530

    Initial Commit

---
+++ src/test/java/com/digitalsystemdreamer/config/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalsystemdreamer.config.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
